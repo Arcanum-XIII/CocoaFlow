7c22f11c1a1bde7c039e4a3099359ed37dcb078b
Sebastien Orban <Arcanum-XIII@users.noreply.github.com>
2016-05-04 10:34:25 +0200

    Initial Commit

---
+++ CocoaFlow/CocoaFlow.h
@@ -0,0 +1,19 @@
+//
+//  CocoaFlow.h
+//  CocoaFlow
+//
+//  Created by Sebastien Orban on 04/05/16.
+//  Copyright © 2016 Random Mechanicals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaFlow.
+FOUNDATION_EXPORT double CocoaFlowVersionNumber;
+
+//! Project version string for CocoaFlow.
+FOUNDATION_EXPORT const unsigned char CocoaFlowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaFlow/PublicHeader.h>
+
+
