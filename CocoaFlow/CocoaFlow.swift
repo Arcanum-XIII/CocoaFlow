c2a55324b0bc2b43fc6dcb27be04978f7ee3edf7
Sebastien Orban <ecrire@sebastienorban.com>
2016-05-24 14:45:57 +0200

    Generic parameter, simplification of the dispatcher, improved documentation

--- CocoaFlow/CocoaFlow.swift
+++ CocoaFlow/CocoaFlow.swift
@@ -9,18 +9,25 @@
 // TODO: add check for recursive mutate/update loop
 import Foundation
 
+// generic class can't be added directly to a dictionnary
 class Item {
+    
 }
 
-/// Usable source (store) for the dispatcher
-class Source<T>:Item {
-    internal let actions:(String, T) -> T?
+/// Source dispatching and management class
+/// Think of this as the middle management : passing data around, and making 
+/// sure that everyone is aware of change.
+/// T is the resulting value you get out of the source, P is the parameter type
+/// used to transact the source
+class Source<T, P>:Item {
+    internal let actions:(String, P) -> T?
     internal let readMethod:() -> T
     var listeners:[NSUUID:(T) -> Void] = [:]
     
-    // actions will be used in a closure that will handle the dispatching.
-    init(actionsList:[String:(T) -> T], read:() -> T) {
-        func evalAction(actionName:String, v:T) -> T? {
+    /// - parameter actionsList: dictionnary of action, action being defined as closure.
+    /// - parameter read: closure to get the current value of the Source
+    init(actionsList:[String:(P) -> T], read:() -> T) {
+        func evalAction(actionName:String, v:P) -> T? {
             if let action = actionsList[actionName] {
                 return action(v)
             }
@@ -31,8 +38,8 @@ class Source<T>:Item {
         super.init()
     }
     
-    // a transaction will notify listener that a new value is there
-    func transact(name:String, value:T) -> T? {
+    /// a transaction will notify listener that a new value is there
+    func transact(name:String, value:P) -> T? {
         let result = actions(name, value)
         if let r = result {
             for (_ ,listener) in listeners {
@@ -42,13 +49,11 @@ class Source<T>:Item {
         return result
     }
     
-    /**
-     Will add a closure listener that will be triggered on transaction
-     
-     - parameter action: closure that will be triggered upon a change. Will receive the new value
-     - returns: closure to remove the listener
-     */
-    func listen(action:(T) -> Void) -> () -> Void {
+     /// Will add a closure listener that will be triggered on transaction
+     ///
+     /// - parameter action: closure that will be triggered upon a change. Will receive the new value
+     /// - returns: closure to remove the listener
+    func addListener(action:(T) -> Void) -> () -> Void {
         let uuid = NSUUID()
         listeners[uuid] = action
         return {
@@ -61,17 +66,9 @@ class Source<T>:Item {
     func read() -> T { return self.readMethod() }
 }
 
-/// Create a dispatcher
-public class FlowDispatcher {
+/// Create a basic dispatcher
+public class Dispatcher {
     var sources: Dictionary<String, Item> = Dictionary()
-    
-    func subscribe<T>(sourceName:String, action:(T) -> Void) -> (() -> Void)? {
-        if let object = sources[sourceName] {
-            let source = object as! Source<T>
-            return source.listen(action)
-        }
-        return nil
-    }
     
     /**
      Register a data source
@@ -85,22 +82,5 @@ public class FlowDispatcher {
     */
     func unregister(name:String) {
         sources.removeValueForKey(name)
-    }
-    
-    // helper function for sources
-    func read<T>(name:String) -> T?{
-        if let object = sources[name] {
-            let source = object as! Source<T>
-            return source.read()
-        }
-        return nil
-    }
-    
-    func transact<T>(name:String, value:T) -> T? {
-        if let object = sources[name] {
-            let source = object as! Source<T>
-            return source.transact(name, value: value)
-        }
-        return nil
     }
 }
